a4d8429dac8d0274a0d9a21d3b84a2d24984d1cf
gnajor <lejonet04@gmail.com>
2025-06-23 21:21:57 +0200

    created api

---
+++ api/handleRequests.ts
@@ -0,0 +1,131 @@
+import { authSpotifyUser, getCountryFromMusicBrainz, getCountryFromWikdata, handleLogout, setToken } from "./utils.ts";
+
+export async function handleRequests(request: Request): Promise<Response>{
+    const url = new URL(request.url);
+    const pathname = url.pathname;
+    const data = await request.json();
+
+    if(pathname === "/api/top-items" && request.method === "GET"){
+        const type = data.type;
+        const range = data.range;
+        const token = await authSpotifyUser(request);
+
+        const url = `https://api.spotify.com/v1/me/top/${type}?time_range=${range}&limit=50&offset=0`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        };
+
+        try {
+            const request = await fetch(url, options);
+            const response = await request.json();
+            const topItems = response.items;
+            return new Response(JSON.stringify(topItems), {status: 200});
+        } 
+        catch(error) {
+            return new Response('Error: ' + error, {status: 500})
+        }
+    }
+
+    if(pathname === "/api/latest-songs" && request.method === "GET"){
+        const days = data.days;
+        const token = await authSpotifyUser(request);
+        const promises = [];
+        
+        const options = {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }
+
+        for(let i = 1; i <= days; i++){
+            const now = Date.now();
+            const secondsAgo = now - (i * 24 * 60 * 60 * 1000);
+            const url = `https://api.spotify.com/v1/me/player/recently-played?limit=50&after=${secondsAgo}`;
+
+            const response = await fetch(url, options);
+            const resource = await response.json();
+            promises.push(resource);
+        }
+
+        try {
+            const responses = await Promise.all(promises);
+            const latestItems = responses.map(response => response.items);
+            return new Response(JSON.stringify(latestItems), {status: 200});
+        } 
+        catch(error) {
+            return new Response('Error: ' + error, {status: 500})
+        }
+    }
+
+    if(pathname === "/api/song-country" && request.method === "GET"){
+        const spotifyId = data.spotifyId;
+        const artistName = data.artistName;
+        const wikidataCountry = await getCountryFromWikdata(spotifyId);
+        const responseData: Record<string, any> = {};
+
+        if (wikidataCountry.results.bindings.length > 0){
+            responseData.source = "wikidata";
+            responseData.result = wikidataCountry;
+        }
+
+        else{
+            const musicbrainzCountry = await getCountryFromMusicBrainz(artistName);
+            responseData.source = "musicbrainz";
+            responseData.result = musicbrainzCountry;
+        }
+
+        return new Response(JSON.stringify(responseData), {status: 200});
+    }
+
+    if(pathname === "/api/set-token" && request.method === "POST"){
+        const code = data.code;
+        const codeVerifier = data.codeVerifier;
+
+        const clientId = Deno.env.get("SPOTIFY_CLIENT_ID");
+        const redirectUri = Deno.env.get("SPOTIFY_REDIRECT_URI");
+
+        if(!clientId){
+            return new Response(JSON.stringify({error: "client_id does not exist"}), {status: 500});
+        }
+
+        if(!redirectUri){
+            return new Response(JSON.stringify({error: "redirect_uri does not exist"}), {status: 500});
+        }
+
+        const body = new URLSearchParams({
+            client_id: clientId,
+            grant_type: 'authorization_code',
+            code,
+            redirect_uri: redirectUri,
+            code_verifier: codeVerifier,
+            scope:"user-read-recently-played"
+        });
+
+
+        try{
+            const response = await fetch("https://accounts.spotify.com/api/token", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body
+            });
+
+            const tokenData = await response.json();
+            return await setToken(tokenData);
+        }
+        catch(error){
+            return new Response('Error: ' + error, {status: 500})
+        }
+    }
+
+    if(pathname === "/api/logout"){
+        return await handleLogout();
+    }
+
+    return new Response('Error: url does not exist', {status: 400});
+}
---
+++ api/utils.ts
@@ -0,0 +1,174 @@
+import { getCookies, setCookie } from "jsr:@std/http/cookie";
+
+export async function getCountryFromWikdata(spotifyId: string): Promise<any | null>{
+    const query = `SELECT ?artist ?artistLabel ?countryLabel 
+                        WHERE {
+                            ?artist wdt:P1902 "${spotifyId}".
+                            OPTIONAL { ?artist wdt:P27 ?country. }
+                            SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+                        }`;    
+    const encodedQuery = encodeURIComponent(query);
+    const url = `https://query.wikidata.org/sparql?query=${encodedQuery}`;
+    const options  = {
+        headers: {
+            'Accept': 'application/sparql-results+json',
+            'User-Agent': 'Visualify/1.0 (leo.muhl04@gmail.com)'
+        }
+    };
+
+    try {
+        const request = await fetch(url, options);
+        const response = await request.json();   
+        return response; 
+    } 
+    catch (error) {
+        console.error('Error', error);
+        return null;
+    }
+}
+
+export async function getCountryFromMusicBrainz(artistName: string): Promise<any | null>{
+    const url = `https://musicbrainz.org/ws/2/artist/?query=artist:"${artistName}"&fmt=json`;
+    const options = {
+        headers: {'User-Agent': 'Visualify/1.0 (lucmov99@gmail.com)'}
+    };
+
+    try {
+        const request = await fetch(url, options);
+        const response = await request.json();
+        const artists = response.artists || [];
+
+        if (artists.length === 0) return null;
+        const exactArtist = artists.find((artist: any) => artist.name.toLowerCase() === artistName.toLowerCase());
+        return exactArtist;    
+    } 
+    catch(error) {
+        console.error('Error:', error);
+        return null;
+    }
+}
+
+export async function setToken(data: Record<string, string>): Promise<Response>{
+    const accessToken = data.accessToken;
+    const refreshToken = data.refreshToken;
+    const headers = new Headers();
+
+    setCookie(headers, {
+        name: "access_token",
+        value: accessToken,
+        httpOnly: true,
+        secure: true,
+        sameSite: "Lax",
+        path: "/",
+        maxAge: 3600
+    });
+
+    setCookie(headers, {
+        name: "refresh_token",
+        value: refreshToken,
+        httpOnly: true,
+        secure: true,
+        sameSite: "Lax",
+        path: "/",
+        maxAge: 3600
+    });
+    
+
+    return new Response(JSON.stringify({success: true}), {status: 200});
+}
+
+export async function handleLogout(): Promise<Response>{
+    return new Response("Logged out", {
+        status: 200,
+        headers: {
+            "Set-Cookie": [
+                "access_token=; HttpOnly; Path=/; Max-Age=0",
+                "refresh_token=; HttpOnly; Path=/; Max-Age=0"
+            ].join("\n"),
+        }
+    });
+}
+
+export async function authSpotifyUser(request: Request): Promise<Response | string>{
+    const cookies = getCookies(request.headers);
+    let accessToken = cookies["access_token"];
+    const refreshToken = cookies["refresh_token"];
+
+    if(!accessToken || !refreshToken){
+        return new Response("unauthorized", {status: 401});
+    }
+
+    let response = await fetch("https://api.spotify.com/v1/me", {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+
+    if(response.status === 401){
+        const refreshed = await refreshAccessToken(refreshToken);
+
+        if(!refreshed){
+            return new Response("Session expired", {status: 401});
+        }
+
+        accessToken = refreshed.access_token;
+
+        const headers = new Headers();
+        setCookie(headers, {
+            name: "access_token",
+            value: accessToken,
+            httpOnly: true,
+            secure: true,
+            sameSite: "Lax",
+            path: "/",
+            maxAge: refreshed.expires_in, 
+        });
+
+        response = await fetch("https://api.spotify.com/v1/me", {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        });
+
+        if(!response.ok){
+            return new Response("Still unauthorized", { status: 401, headers });
+        }
+        return accessToken;
+    }
+    return accessToken;
+}
+
+async function refreshAccessToken(refreshToken: string): Promise<null | { access_token: string, expires_in: number }>{
+    const clientId = Deno.env.get("SPOTIFY_CLIENT_ID");
+
+    if(!clientId){
+        return null;
+    }
+
+    const body = new URLSearchParams({
+        grant_type: "refresh_token",
+        refresh_token: refreshToken,
+        client_id: clientId,
+    });
+
+    try{
+        const response = await fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body,
+        });
+
+        const data = await response.json();
+        return {
+            access_token: data.access_token,
+            expires_in: data.expires_in
+        }
+    }
+    catch(error){
+        console.error(error);
+        return null;
+    }    
+}
+
