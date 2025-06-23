9be4b736803b976e80023ece6b7850db8de46a22
gnajor <lejonet04@gmail.com>
2025-06-23 21:22:48 +0200

    deno server finished

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,14 @@
 import { serveDir} from "jsr:@std/http/file-server";
+import { handleRequests } from "../api/handleRequests.ts";
 
 export function serverRequests(request: Request){
     const pathname = new URL(request.url).pathname;
+
+    console.log(pathname)
+
+    if(pathname.startsWith("/api/")){
+        return handleRequests(request);
+    }
 
     return serveDir(request, {
         fsRoot: "public",
@@ -9,4 +16,4 @@ export function serverRequests(request: Request){
     });
 }
 
-Deno.serve(serverRequests);
+Deno.serve({ port: 8888, hostname: "127.0.0.1" }, serverRequests);
