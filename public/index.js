57ba8890d36a6c637041d491499ae1b144550957
gnajor <lejonet04@gmail.com>
2025-08-13 22:09:43 +0200

    Trying out Deno.deploy

--- public/index.js
+++ public/index.js
@@ -1,13 +1,16 @@
 import { apiCom } from "./apiCom/apiCom.js";
+import { updateNavMarker } from "./components/header/nav/nav.js";
+import { Switch } from "./components/header/switch/switch.js";
 import { handleRedirect } from "./logic/handleRedirect.js";
 import { getAllTopUserDataAndSetState } from "./logic/utils.js";
 import { renderHomePage } from "./pages/homePage/homePage.js";
+import { updateArtistDivPosition } from "./pages/mainPage/decadesPage/decadesPage.js";
 import { renderStructure } from "./pages/mainPage/structure.js";
 
 
 export const State = {
     clientId: "aa99b24e94d448eab167b514b89f2de2",
-    redirectUri: "http://127.0.0.1:8888/",
+    redirectUri: "https://visualify.deno.dev/"/* "http://127.0.0.1:8888/" */,
     userData: {
         artists:{
             short_term: null,
@@ -40,7 +43,14 @@ const app = {
             await getAllTopUserDataAndSetState();
             renderStructure(this.parent);
         }
+    },
+
+    onResizeWindow(){
+        Switch.updateSwitchMarker();
+        updateNavMarker();
+        updateArtistDivPosition();
     }
 }
 
+window.onresize = app.onResizeWindow;
 app.start();
