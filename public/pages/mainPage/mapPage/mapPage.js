98a6d5d354125583570fc9df928972b582704e47
gnajor <lejonet04@gmail.com>
2025-08-31 21:36:51 +0200

    The map is done

--- public/pages/mainPage/mapPage/mapPage.js
+++ public/pages/mainPage/mapPage/mapPage.js
@@ -1,80 +1,203 @@
 import { getMapData } from "../../../logic/utils.js";
 import { apiCom } from "../../../apiCom/apiCom.js";
+import { Selector } from "../../../components/header/selector/selector.js";
 
 export function renderMapPage(parent){
     const dataset = getMapData();
 
     const diagramContainer = document.createElement("div");
+    const songContainer = document.createElement("div");
     diagramContainer.className = "diagram-container";
+    songContainer.className = "song-container";
     parent.appendChild(diagramContainer);
+    parent.appendChild(songContainer);
 
+    const selectorInstance = Selector.getSelectorByPageId(parent.id);
+    const map = new Map(diagramContainer, dataset, "short_term", selectorInstance);
 
-    const map = new Map(diagramContainer, dataset["short_term"]);
+    document.addEventListener("radar:done", async (event) => {
+        if(event.detail.chartId === "moods" && event.detail.firstTime){
+            document.dispatchEvent(new CustomEvent("map:processing", {detail: {chartId: "map"}}));
+            await map.fetchAndSetColors();
+            selectorInstance.enable();
+            document.dispatchEvent(new CustomEvent("map:done", {detail: {chartId: "map"}}));
+        }
+        else if(event.detail.chartId === "moods" && !event.detail.firstTime){
+            selectorInstance.enable();
+        }
+    });
 
-/*     const data = await apiCom("song:get-country", {
-        spotifyId: artist.id,
-        artistName: artist.name
-    }); */
+    document.addEventListener("radar:processing", async (event) => {
+        if(event.detail.chartId === "moods"){
+            selectorInstance.disable();
+        }
+    });
+
+    selectorInstance.event((event) => {
+        map.changeData(dataset, event.target.value);
+    }); 
 }
 
 class Map{
-    constructor(parent, dataset){
+    constructor(parent, dataset, range, selectorInstance){
+        this.selectorInstance = selectorInstance;
         this.parent = d3.select(parent);
-        this.dataset = dataset;
+        this.range = range;
+        this.dataset = dataset[range];
 
-        this.hSvg = 1000;
+        this.existingData = {
+            "short_term": [],
+            "medium_term": [],
+            "long_term": []
+        } 
+
+        this.hSvg = 800;
         this.wSvg = 1500;
+        this.marginBottom = 40;
+        this.colors = [
+            "#E3CCFF",
+            "#c89ffdff",
+            "#AD74F6",
+            "#9745FF"
+        ]
 
         this.init();
     }
 
+    sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
     async init(){
         this.svg = this.parent.append("svg")
-            .attr("height", "100%")
             .attr("width", "100%")
-            .attr("viewbox", `0 0 ${this.wSvg} ${this.hSvg}`)
+            .attr("height", "95%")
+            .attr("viewBox", `0 0 ${this.wSvg} ${this.hSvg}`)
             .classed("map-graph", true)
-
+     
+        await this.prepCountryData();
         this.prepScales();
-        await this.prepData();
         this.render();
+        this.selectorInstance.disable();
+    }
+
+    async prepCountryData(){
+        const data = await d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson");
+        this.countries = data.features.filter(country => country.properties.name !== "Antarctica");
+
+        this.world = { type: "FeatureCollection", features: this.countries };
     }
 
     prepScales(){
-        const path = d3.geoPath();
-        this.projection = d3.geoMercator()
-            .scale(180)
-            .center([0, 20])
-            .translate([this.wSvg/1.5, this.hSvg/2]);
-    }
-
-    async prepData(){
-        const data = await d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson");
-        this.countries = data.features;
-
-        const index = this.countries.findIndex(country => country.properties.name === "Antarctica")
-
-        if(index !== -1){
-            this.countries.splice(index, 1);
+        this.projection = d3.geoMercator().fitSize([this.wSvg, this.hSvg - this.marginBottom], this.world);
+        this.path = d3.geoPath().projection(this.projection);
+        this.customInterpolator = (t) => {
+            return d3.interpolateRgbBasis(this.colors)(t);
         }
     }
 
     render(){
-        console.log(this.countries)
-
         this.svg.append("g").classed("graph", true).selectAll("path")
             .data(this.countries)
             .enter()
             .append("path")
-                .attr("d", d3.geoPath().projection(this.projection))
+                .attr("d", this.path)
                 .attr("id", d => this.formatCountryName(d.properties.name))
-                .style('fill', '#383141')
-                .style("stroke", "black")
-
+                .attr('fill', '#383141')
+                .attr("stroke", "black")
     }
+
+    async fetchAndSetColors(){
+        for(const artist of this.dataset){
+            const data = await apiCom("song:get-country", {spotifyId: artist.id, artistName: artist.name})
+            this.formatArtistCountryItem(data, artist.name);
+        }
+    }
+
+    formatArtistCountryItem(data, artist){
+        if(data.ok && data.resource){
+            if(data.resource.result){
+                const country = data.resource.result;
+                const exists = this.existingData[this.range].find(item => item.country === country);
+                let newCountry = null
+
+                if(exists){
+                    exists.value++;
+                    newCountry = exists
+                }
+                else{
+                    newCountry = {
+                        "country": country,
+                        "name": artist,
+                        "value": 1
+                    }
+                    this.existingData[this.range].push(newCountry);
+                }
+
+                this.updateCountryColor(newCountry);
+            }
+        }
+    } 
+
+    updateCountryColor(countryObj){
+        const max = d3.max(this.existingData[this.range].map(item => item.value))
+        const min = d3.min(this.existingData[this.range].map(item => item.value))
+        const countryName = this.formatCountryName(countryObj.country);
+        const countryValue = countryObj.value;
+
+        const color = d3.scaleSequential()
+            .domain([min, max])
+            .interpolator(this.customInterpolator);
+
+        this.svg.select(`#${countryName}`)
+            .classed('done', true)
+            .transition()
+            .duration(300)
+            .attr("fill", d => color(countryValue))
+    }
+
 
     formatCountryName(country){
-        if(country.includes(" ")) return name.replaceAll(" ", "-");
-        return country;
+        let formatted = country;
+
+        if(country === "United States"){
+            formatted = "USA";
+        }
+
+        else if (country === "United Kingdom"){
+            formatted = "England";
+        }
+
+        else if(country.includes(" ")){
+            formatted = country.replaceAll(" ", "-");
+        } 
+        return formatted.toLowerCase();
     }
+
+    setColorsFromMemory(){
+        for(const item of this.existingData[this.range]){
+            this.updateCountryColor(item);
+        }    
+    }
+
+    async changeData(dataset, range){
+        this.dataset = dataset[range];
+        this.range = range;
+
+        this.svg.selectAll("path")
+            .attr('fill', '#383141')
+            .attr("stroke", "black")
+            .classed("done", false)
+        
+            if(this.existingData[this.range].length !== 0){
+                this.setColorsFromMemory();
+            }
+            else{
+                this.selectorInstance.disable();
+                document.dispatchEvent(new CustomEvent("map:processing", {detail: {chartId: "map"}}));
+                await this.fetchAndSetColors();
+                document.dispatchEvent(new CustomEvent("map:done", {detail: {chartId: "map"}}));
+                this.selectorInstance.enable();
+            }
+    }    
 }
