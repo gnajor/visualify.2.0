bab69d2a32ee20b5034f33f4a8d68bc34a5d5153
gnajor <lejonet04@gmail.com>
2025-08-31 21:44:15 +0200

    Have created a loading class but don't know if I'll use it

---
+++ public/components/loading/loading.js
@@ -0,0 +1,16 @@
+class Loading{
+    constructor(parent){
+        this.parent = parent;
+        this.barColor = "var(--main-green-color)";
+
+    }
+
+
+    renderLoadingBar(){
+
+    }
+
+    renderLoadingWithBars(){
+        this.parent = ""
+    }
+}
