3396c8f1d31f4097fd41752751751c38cb3370fa
gnajor <lejonet04@gmail.com>
2025-09-23 23:08:49 +0200

    edited the scheme

--- db/scheme.sql
+++ db/scheme.sql
@@ -1,15 +1,4 @@
-CREATE TABLE IF NOT EXISTS Song(
-    id SERIAL PRIMARY KEY,
-    artist_id INT NOT NULL
-    title VARCHAR(200) NOT NULL,
-    image TEXT NOT NULL,
-    year INT NOT NULL,
-    popularity INT NOT NULL,
-
-    artist_id REFERENCES Artist(id) ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS Artist(
+CREATE TABLE IF NOT EXISTS Artist (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL, 
     image TEXT NOT NULL,
@@ -17,31 +6,31 @@ CREATE TABLE IF NOT EXISTS Artist(
     country VARCHAR(100)
 );
 
-CREATE TABLE IF NOT EXISTS Mood(
+CREATE TABLE IF NOT EXISTS Song (
+    id SERIAL PRIMARY KEY,
+    artist_id INT NOT NULL REFERENCES Artist(id) ON DELETE CASCADE,
+    title VARCHAR(200) NOT NULL,
+    image TEXT NOT NULL,
+    year INT NOT NULL,
+    popularity INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Mood (
     type VARCHAR(50) PRIMARY KEY
 );
 
-CREATE TABLE IF NOT EXISTS Genre(
+CREATE TABLE IF NOT EXISTS Genre (
     name VARCHAR(100) PRIMARY KEY
 );
 
-CREATE TABLE IF NOT EXISTS Song_moods(
-    mood_type VARCHAR(50),
-    song_id INT,
-
-    song_id REFERENCES Song(id) ON DELETE CASCADE,
-    mood_type REFERENCES Mood(type) ON DELETE CASCADE,
-    PRIMARY KEY(mood_type, song_id)
+CREATE TABLE IF NOT EXISTS Song_moods (
+    mood_type VARCHAR(50) NOT NULL REFERENCES Mood(type) ON DELETE CASCADE,
+    song_id INT NOT NULL REFERENCES Song(id) ON DELETE CASCADE,
+    PRIMARY KEY (mood_type, song_id)
 );
 
-CREATE TABLE IF NOT EXISTS Song_genres(
-    genre_name VARCHAR(100),
-    song_id INT,
-
-    song_id REFERENCES Song(id) ON DELETE CASCADE,
-    mood_type REFERENCES Mood(type) ON DELETE CASCADE,
-    PRIMARY KEY(mood_type, song_id)
+CREATE TABLE IF NOT EXISTS Song_genres (
+    genre_name VARCHAR(100) NOT NULL REFERENCES Genre(name) ON DELETE CASCADE,
+    song_id INT NOT NULL REFERENCES Song(id) ON DELETE CASCADE,
+    PRIMARY KEY (genre_name, song_id)
 );
-
-
-
