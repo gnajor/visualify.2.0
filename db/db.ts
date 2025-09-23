e30061e3c4dc20202dab8aac4ef5a78c73519a83
gnajor <lejonet04@gmail.com>
2025-09-23 23:09:12 +0200

    initiated db

--- db/db.ts
+++ db/db.ts
@@ -1,7 +1,30 @@
 import { createClient } from "jsr:@supabase/supabase-js@2";
-    const supabase = createClient(
+import { Song } from "./interfaces.ts";
+
+
+const supabase = createClient(
     Deno.env.get("SUPABASE_URL")!,
     Deno.env.get("SUPABASE_KEY")!,
-
-    
 );
+
+export async function insertSongs(songs: Song[]) {
+    if(songs.length === 0) return;
+
+}
+
+
+
+export async function supabaseSetup(): Promise<any>{
+
+    const resource = await supabase
+        .from("mood")
+        .insert([{ 
+            type: "Calm"
+        }])
+        .select();
+
+
+    if (resource.error) {
+        console.error(resource.error);
+    }
+}
