fbe783fab34c4c9c0e0aa72650e99bd4791bcda5
gnajor <lejonet04@gmail.com>
2026-01-19 23:05:02 +0100

    The db scheme updated

--- db/scheme.sql
+++ db/scheme.sql
@@ -1,18 +1,32 @@
 CREATE TABLE IF NOT EXISTS Artist (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL, 
+    id VARCHAR(22) PRIMARY KEY,
+    name TEXT NOT NULL,
+    link TEXT NOT NULL, 
     image TEXT,
     popularity INT,
-    country VARCHAR(100)
+    country VARCHAR(100),
+    country_tries INT
+);
+
+CREATE TABLE IF NOT EXISTS Album (
+    id VARCHAR(22) PRIMARY KEY,
+    artist_id VARCHAR(22) NOT NULL REFERENCES Artist(id) ON DELETE CASCADE,
+    image TEXT NOT NULL,
+    release_year INT NOT NULL,
+    name TEXT NOT NULL,
+    link TEXT NOT NULL,
+    total_tracks INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Song (
-    id SERIAL PRIMARY KEY,
-    artist_id INT NOT NULL REFERENCES Artist(id) ON DELETE CASCADE,
+    id VARCHAR(22) PRIMARY KEY,
+    artist_id VARCHAR(22) NOT NULL REFERENCES Artist(id) ON DELETE CASCADE,
+    album_id VARCHAR(22) NOT NULL references Album(id) ON DELETE CASCADE,
     title VARCHAR(200) NOT NULL,
-    image TEXT NOT NULL,
-    year INT NOT NULL,
-    popularity INT NOT NULL
+    popularity INT NOT NULL,
+    link TEXT NOT NULL,
+    duration INT NOT NULL,
+    explicit BOOLEAN NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Mood (
@@ -25,12 +39,12 @@ CREATE TABLE IF NOT EXISTS Genre (
 
 CREATE TABLE IF NOT EXISTS Song_moods (
     mood_type VARCHAR(50) NOT NULL REFERENCES Mood(type) ON DELETE CASCADE,
-    song_id INT NOT NULL REFERENCES Song(id) ON DELETE CASCADE,
+    song_id VARCHAR(22) NOT NULL REFERENCES Song(id) ON DELETE CASCADE,
     PRIMARY KEY (mood_type, song_id)
 );
 
-CREATE TABLE IF NOT EXISTS Song_genres (
+CREATE TABLE IF NOT EXISTS artist_genres (
     genre_name VARCHAR(100) NOT NULL REFERENCES Genre(name) ON DELETE CASCADE,
-    song_id INT NOT NULL REFERENCES Song(id) ON DELETE CASCADE,
-    PRIMARY KEY (genre_name, song_id)
+    artist_id VARCHAR(22) NOT NULL REFERENCES Artist(id) ON DELETE CASCADE,
+    PRIMARY KEY (genre_name, artist_id)
 );
